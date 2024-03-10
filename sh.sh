26a727f6ad3ee7b0f091e4025ca38343c9804768
bappsd <rarog.dos@gmail.com>
2024-03-10 16:02:40 +0100

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -4,5 +4,5 @@ USERNAME="user101"
 PASSWORD="123123zaq*"
 
 while IFS= read -r url; do
-    wget --user="$USERNAME" --password="$PASSWORD" "$url"
+    wget --user="$USERNAME" --password="$PASSWORD" "$url" -nc
 done < urls
